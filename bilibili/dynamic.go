34de07d56da3b0c96fbe8b581298383780aae2dc
shynome <shynome@gmail.com>
2022-12-08 12:28:25 +0800

    调整屏幕尺寸

--- bilibili/dynamic.go
+++ bilibili/dynamic.go
@@ -28,7 +28,7 @@ func GetDynamicPic(c echo.Context) (err error) {
 	var img []byte
 	tasks := chromedp.Tasks{
 		emulation.SetUserAgentOverride("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1").WithPlatform("iPhone"),
-		emulation.SetDeviceMetricsOverride(320, 640, 3.0, true),
+		emulation.SetDeviceMetricsOverride(400, 800, 2.5, true),
 		chromedp.Navigate(link),
 		chromedp.WaitReady(".dyn-card *"),
 		chromedp.Evaluate(getClearElemJs, nil),
